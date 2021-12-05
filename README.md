1bc5285333e50de351a29b85513182679a10ba32
GangemiLorenzo <lorenzo.gangemi.developer@gmail.com>
2021-12-05 16:35:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# personal_portfolio
+
+Portfolio app built in Flutter for mobile/web/desktop.
---
+++ android/app/src/main/kotlin/com/example/personal_portfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.personal_portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
