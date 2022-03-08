f6f8591b63d7bacf1dfd0c9009386a96985ec4b1
GangemiLorenzo <lorenzo.gangemi.developer@gmail.com>
2022-03-09 00:04:42 +0100

    removes build folder

--- lib/common/common.dart
+++
@@ -1,4 +0,0 @@
-export 'app_scaffold.dart';
-export 'constants.dart';
-export 'stroke_grid_pattern.dart';
-export 'utils.dart';
--- lib/theme/theme.dart
+++
@@ -1,5 +0,0 @@
-export 'palette.dart';
-export 'theme_data.dart';
-export 'theme_extension.dart';
-export 'theme_repo.dart';
-export 'typography.dart';
