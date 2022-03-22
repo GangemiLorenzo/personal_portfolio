8630e68c34c888c3a5b6739d339e6e117bc9f41a
GangemiLorenzo <lorenzo.gangemi.developer@gmail.com>
2022-03-22 23:50:58 +0100

    Add animation to skill indicator

--- lib/common/skill_indicator.dart
+++ lib/common/skill_indicator.dart
@@ -1,6 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class SkillIndicator extends StatelessWidget {
+class SkillIndicator extends StatefulWidget {
   const SkillIndicator({
     required this.color,
     required this.backgroundColor,
@@ -17,6 +19,36 @@ class SkillIndicator extends StatelessWidget {
   final Color? iconColor;
 
   @override
+  State<SkillIndicator> createState() => _SkillIndicatorState();
+}
+
+class _SkillIndicatorState extends State<SkillIndicator>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation _animation;
+  late int _progress;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        duration: const Duration(milliseconds: 800), vsync: this);
+    _animation = IntTween(begin: -10, end: 10).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+    _progress = widget.progress * 10 + _animation.value as int;
+    int delay = Random().nextInt(500) + 500;
+    Future.delayed(Duration(milliseconds: delay), () {
+      _animationController.repeat(reverse: true);
+    });
+    _animation.addListener(() {
+      setState(() {
+        _progress = widget.progress * 10 + _animation.value as int;
+      });
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -26,23 +58,23 @@ class SkillIndicator extends StatelessWidget {
             child: Container(
               height: 2,
               decoration: BoxDecoration(
-                color: backgroundColor,
+                color: widget.backgroundColor,
                 borderRadius: const BorderRadius.all(Radius.circular(5.0)),
               ),
               child: Row(
                 children: [
                   Expanded(
-                    flex: progress,
+                    flex: _progress,
                     child: Container(
                       decoration: BoxDecoration(
-                        color: color,
+                        color: widget.color,
                         borderRadius:
                             const BorderRadius.all(Radius.circular(5.0)),
                       ),
                     ),
                   ),
                   Expanded(
-                    flex: 100 - progress,
+                    flex: 1000 - _progress,
                     child: Container(),
                   ),
                 ],
@@ -53,8 +85,8 @@ class SkillIndicator extends StatelessWidget {
             width: 16.0,
           ),
           Icon(
-            icon,
-            color: iconColor,
+            widget.icon,
+            color: widget.iconColor,
           )
         ],
       ),
